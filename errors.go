26e9e47f5d7a23846c0ed1b1c8934f45bd790e6b
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-19 10:38:47 +0300

    Added CannotSetRLimitNoFile

--- errors.go
+++ errors.go
@@ -5,18 +5,19 @@ import (
 )
 
 var (
-	NotImplemented       SmartError = &smartError{error: fmt.Errorf("Not implemented (yet?)")}
-	UnableToConnect      SmartError = &smartError{error: fmt.Errorf("Unable to connect")}
-	ProtocolMismatch     SmartError = &smartError{error: fmt.Errorf("Protocol mismatch")}
-	NotFound             SmartError = &smartError{error: fmt.Errorf("Not found")}
-	OutOfRange           SmartError = &smartError{error: fmt.Errorf("Out of range")}
-	CannotResolveAddress SmartError = &smartError{error: fmt.Errorf("Cannot resolve the address")}
-	CannotWriteToFile    SmartError = &smartError{error: fmt.Errorf("Cannot write to the file")}
-	CannotParseFile      SmartError = &smartError{error: fmt.Errorf("Cannot parse the file")}
-	CannotOpenFile       SmartError = &smartError{error: fmt.Errorf("Cannot open the file")}
-	CannotSendData       SmartError = &smartError{error: fmt.Errorf("Cannot send the data")}
-	UnableToGetKey       SmartError = &smartError{error: fmt.Errorf("Unable to get a key")}
-	UnableToStartSession SmartError = &smartError{error: fmt.Errorf("Unable to start a session")}
-	UnableToListen       SmartError = &smartError{error: fmt.Errorf("Unable to start listening")}
-	UnableToParse        SmartError = &smartError{error: fmt.Errorf("Unable to parse")}
+	NotImplemented        SmartError = &smartError{error: fmt.Errorf(`Not implemented (yet?)`)}
+	UnableToConnect       SmartError = &smartError{error: fmt.Errorf(`Unable to connect`)}
+	ProtocolMismatch      SmartError = &smartError{error: fmt.Errorf(`Protocol mismatch`)}
+	NotFound              SmartError = &smartError{error: fmt.Errorf(`Not found`)}
+	OutOfRange            SmartError = &smartError{error: fmt.Errorf(`Out of range`)}
+	CannotResolveAddress  SmartError = &smartError{error: fmt.Errorf(`Cannot resolve the address`)}
+	CannotWriteToFile     SmartError = &smartError{error: fmt.Errorf(`Cannot write to the file`)}
+	CannotParseFile       SmartError = &smartError{error: fmt.Errorf(`Cannot parse the file`)}
+	CannotOpenFile        SmartError = &smartError{error: fmt.Errorf(`Cannot open the file`)}
+	CannotSendData        SmartError = &smartError{error: fmt.Errorf(`Cannot send the data`)}
+	CannotSetRLimitNoFile SmartError = &smartError{error: fmt.Errorf(`Cannot set limit "nofile"`)}
+	UnableToGetKey        SmartError = &smartError{error: fmt.Errorf(`Unable to get a key`)}
+	UnableToStartSession  SmartError = &smartError{error: fmt.Errorf(`Unable to start a session`)}
+	UnableToListen        SmartError = &smartError{error: fmt.Errorf(`Unable to start listening`)}
+	UnableToParse         SmartError = &smartError{error: fmt.Errorf(`Unable to parse`)}
 )
